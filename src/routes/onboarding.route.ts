c900af4a6fb6482a67182dd07f22ed898ada45d1
GandresCoello18 <goyeselcoca@gmail.com>
2025-07-31 00:22:07 -0500

    remove middelware auth in list question onboarding

--- src/routes/onboarding.route.ts
+++ src/routes/onboarding.route.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { getQuestionsOnboarding } from '../modules/onboarding/onboarding.controller';
-import { authenticate } from '../middleware/auth.middleware';
 
 const authRoutes = Router();
 const pathAuth = '/onboarding';
 
-authRoutes.get(`${pathAuth}/questions/list`, authenticate, getQuestionsOnboarding);
+authRoutes.get(`${pathAuth}/questions/list`, getQuestionsOnboarding);
 
 export default authRoutes;
