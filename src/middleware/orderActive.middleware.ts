2e7368e44470903e6da8bccff73f1ddce58dbfab
GandresCoello18 <goyeselcoca@gmail.com>
2025-08-08 00:10:08 -0500

    fix middelware activeOrder

--- src/middleware/orderActive.middleware.ts
+++ src/middleware/orderActive.middleware.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { OrderMongoModel, SelectStatusOrderModel } from '../modules/order/order.model';
 
-export const activeOrder =
-  () =>
-  async (req: Request, res: Response, next: NextFunction) => {
+export const activeOrder = async (req: Request, res: Response, next: NextFunction) => {
     const user = req.user;
     const orderActive = await OrderMongoModel.countDocuments({ userId: user._id, status: SelectStatusOrderModel.Approved });
     if (!orderActive) {
