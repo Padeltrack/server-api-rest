d12709289196d64eea86f166becb95f8939288dc
GandresCoello18 <goyeselcoca@gmail.com>
2025-09-06 00:12:07 -0500

    sort get coach and students search

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -154,15 +154,22 @@ export const getCoachOrStudentUsers = async (req: Request, res: Response) => {
   req.logger.info({ status: 'start' });
 
   try {
-    const me = req.user;
+    const me = { _id: "68a5ab995dd8c33aa9b18c06", role: SelectRoleModel.Student }// req.user;
     const page = Number(req.query?.page) || 1;
     const limit = Number(req.query?.limit) || 10;
     const search = req.query?.search || '';
     const skip = (page - 1) * limit;
     let role: RoleModel = SelectRoleModel.Student;
+    let ids: string[] = [];
 
     if (me.role === SelectRoleModel.Student) {
       role = SelectRoleModel.Coach;
+      const gerAssign = await StudentCoachesMongoModel.find({ studentId: me._id }).select('coachId');
+      ids = gerAssign.map(_id => _id.coachId);
+    } else {
+      role = SelectRoleModel.Student;
+      const gerAssign = await StudentCoachesMongoModel.find({ coachId: me._id }).select('studentId');
+      ids = gerAssign.map(_id => _id.studentId);
     }
 
     const query: any = { role };
@@ -176,8 +183,9 @@ export const getCoachOrStudentUsers = async (req: Request, res: Response) => {
     }
 
     const users = await UserMongoModel.find(query).skip(skip).limit(limit).sort({ createdAt: -1 });
+    const usersSort = ids.map(id => users.find(u => u._id === id));
 
-    return res.status(200).json({ users });
+    return res.status(200).json({ users: usersSort });
   } catch (error) {
     req.logger.error({ status: 'error', code: 500, error: error.message });
     return res.status(401).json({ message: 'Error getting users' });
