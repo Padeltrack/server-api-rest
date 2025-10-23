79ab59e28f98d8e86941cca90d842f6c467be1f7
mtc-acoello <acoello@mimstechcorp.com>
2025-10-23 00:37:07 -0500

    add url domain in cors permits

--- src/shared/util/url.util.ts
+++ src/shared/util/url.util.ts
@@ -1,11 +1,11 @@
 // URLS
 const HOST_CLIENT_ADMIN_LOCAL = 'http://localhost:5173';
 export const HOST_CLIENT_ADMIN_PROD = 'https://admin.padeltrack.app';
-export const HOST_CLIENT_PUBLIC_LOCAL = '';
+export const HOST_CLIENT_ADMIN_DEV = 'https://dashboard-padel-track-git-develop-padels-projects-a81ffce3.vercel.app';
 export const HOST_CLIENT_PUBLIC_PROD = '';
 
 // CLIENT
-export const HOST_ADMINS = [HOST_CLIENT_ADMIN_LOCAL, HOST_CLIENT_ADMIN_PROD];
-export const HOST_CLIENTS = [HOST_CLIENT_PUBLIC_LOCAL, HOST_CLIENT_PUBLIC_PROD];
+export const HOST_ADMINS = [HOST_CLIENT_ADMIN_LOCAL, HOST_CLIENT_ADMIN_PROD, HOST_CLIENT_ADMIN_DEV];
+export const HOST_CLIENTS = [];
 
 export const HOST_PERMITS = [...HOST_ADMINS, ...HOST_CLIENTS];
